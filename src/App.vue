2e535dc8271b15b87df2ed3dc49f866b4b22741c
nk-coding <niklask.coding@gmail.com>
2022-07-14 13:23:09 +0200

    maybe fix for weird frontend bug

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <template>
     <v-app>
         <v-main>
-            <router-view/>
+            <router-view />
         </v-main>
-        <v-bottom-navigation
-            :value="$route.name"
-            color="primary"
-        >
+        <v-bottom-navigation :value="$route.name" color="primary">
             <v-btn value="discover" to="/discover">
                 <span>Discover</span>
                 <v-icon>mdi-map-marker</v-icon>
@@ -25,14 +22,32 @@
 
 <script lang="ts">
 import { Vue, Options } from 'vue-class-component';
+import debounce from "lodash/debounce"
 
 @Options({})
 export default class App extends Vue {
     route = "home"
+
+    setViewHeight() {
+        const vh = window.innerHeight * 0.01
+        document.documentElement.style.setProperty('--vh', `${vh}px`)
+    }
+
+    mounted() {
+        this.setViewHeight()
+        const debouncedSetHeight = debounce(this.setViewHeight, 50)
+        window.addEventListener('resize', debouncedSetHeight)
+    }
 }
 </script>
 <style>
-html,body {
+html,
+body {
     overflow-y: hidden !important;
+}
+
+.v-application--wrap {
+    min-height: 100vh;
+    min-height: calc(var(--vh, 1vh) * 100);
 }
 </style>
