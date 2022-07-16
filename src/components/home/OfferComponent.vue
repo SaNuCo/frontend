2ce8e6f0941079ef6212be8551884f636c5c2788
PiaWippermann <pia.wippermann@gmx.de>
2022-07-16 12:35:28 +0200

    Test for list of extisting offers

--- src/components/home/OfferComponent.vue
+++ src/components/home/OfferComponent.vue
@@ -8,24 +8,83 @@
 
     <!-- List of all existing offers -->
     <div class="offer__container">
-      <!-- TODO: durch gegebene Angebote loopen, Bilder und Name rendern -->
+      <div
+        class="offer__items"
+        v-for="(existingOffer, offerKey) in existingOffers"
+        v-bind:key="offerKey"
+      >
+        {{ existingOffer.title }}
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
+import gql from "graphql-tag";
 
 @Options({
   components: {},
 })
-export default class OfferComponent extends Vue {}
+export default class OfferComponent extends Vue {
+  existingOffers = [
+    {
+      title: "Tomaten",
+      picture: "",
+      location: "",
+      category: "share",
+      description: "Biete Tomaten",
+    },
+    {
+      title: "Ananas",
+      picture: "",
+      location: "",
+      category: "share",
+      description: "Biete Tomaten",
+    },
+    {
+      title: "Feta",
+      picture: "",
+      location: "",
+      category: "share",
+      description: "Biete Tomaten",
+    },
+  ];
+
+  created() {
+    this.fetchExistingOffers();
+  }
+
+  async fetchExistingOffers() {
+    const existingOffers = await this.$apollo.query({
+      query: gql`
+        query ($username: String!) {
+          foodOfferings(filter: { user: { username: { eq: $username } } }) {
+            nodes {
+              id
+              title
+              picture
+              category
+              description
+            }
+          }
+        }
+      `,
+    });
+    console.log("existingOffers", existingOffers);
+  }
+}
 </script>
 
 <style lang="scss">
 .offer {
   &__container {
     padding: 2rem;
+  }
+
+  &__items {
+    padding: 1rem;
+    border-bottom: 1px solid lightgray;
   }
 }
 </style>
--- src/views/OfferingView.vue
+++ src/views/OfferingView.vue
@@ -1,19 +1,35 @@
 <template>
-    <DialogViewComponent :name="offeringId == null ? 'Create Offering' : 'Update Offering'">
-        <template #default>
-            <div class="ma-3 pa-3">
-                <v-text-field placeholder="Title" v-model="title"></v-text-field>
-                <v-select label="Category" v-model="category" :items="categories" />
-                <ImageUpload @imageChanged="imageChanged" @deleteImage="deleteImage" :value="imageId"></ImageUpload>
-                <v-textarea placeholder="Description" v-model="description"></v-textarea>
-                <MapComponent class="location-picker mb-5" :pickerMode="true" @pickerUpdated="pickerUpdated"
-                    :pickerCoordinates="location" />
-                <v-btn class="save-button " :disabled="!title || !location" @click="save">{{ offeringId == null ?
-                        "Create" : "Update"
-                }}</v-btn>
-            </div>
-        </template>
-    </DialogViewComponent>
+  <DialogViewComponent
+    :name="offeringId == null ? 'Create Offering' : 'Update Offering'"
+  >
+    <template #default>
+      <div class="ma-3 pa-3">
+        <v-text-field placeholder="Title" v-model="title"></v-text-field>
+        <v-select label="Category" v-model="category" :items="categories" />
+        <ImageUpload
+          @imageChanged="imageChanged"
+          @deleteImage="deleteImage"
+          :value="imageId"
+        ></ImageUpload>
+        <v-textarea
+          placeholder="Description"
+          v-model="description"
+        ></v-textarea>
+        <MapComponent
+          class="location-picker mb-5"
+          :pickerMode="true"
+          @pickerUpdated="pickerUpdated"
+          :pickerCoordinates="location"
+        />
+        <v-btn
+          class="save-button"
+          :disabled="!title || !location"
+          @click="save"
+          >{{ offeringId == null ? "Create" : "Update" }}</v-btn
+        >
+      </div>
+    </template>
+  </DialogViewComponent>
 </template>
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
@@ -23,81 +39,82 @@ import MapComponent from "../components/map/MapComponent.vue";
 import gql from "graphql-tag";
 
 @Options({
-    components: {
-        DialogViewComponent,
-        ImageUpload,
-        MapComponent
-    }
+  components: {
+    DialogViewComponent,
+    ImageUpload,
+    MapComponent,
+  },
 })
 export default class OfferingView extends Vue {
+  imageId: string | null = null;
+  title = "";
+  description = "";
+  category = "share";
+  location: [number, number] | null = null;
 
-    imageId: string | null = null
-    title = ""
-    description = ""
-    category = "share"
-    location: [number, number] | null = null
+  categories = [
+    {
+      value: "share",
+      title: "Share your food",
+    },
+    {
+      value: "together",
+      title: "Cook and eat together",
+    },
+  ];
 
-    categories = [
-        {
-            value: "share",
-            title: "Share your food"
-        }, {
-            value: "together",
-            title: "Cook and eat together"
-        }
-    ]
+  get offeringId(): string | null {
+    return (this.$route.params.id ?? null) as string | null;
+  }
 
-    get offeringId(): string | null {
-        return (this.$route.params.id ?? null) as string | null
+  imageChanged(id: string) {
+    this.imageId = id;
+  }
+
+  deleteImage() {
+    this.imageId = null;
+  }
+
+  pickerUpdated(coordinates: [number, number]) {
+    this.location = coordinates;
+  }
+
+  async save(): Promise<void> {
+    console.log("location", this.location);
+    if (this.offeringId == null) {
+      await this.$apollo.mutate({
+        mutation: gql`
+          mutation ($input: FoodOfferingInput!) {
+            createFoodOffering(input: $input) {
+              id
+            }
+          }
+        `,
+        variables: {
+          input: {
+            title: this.title,
+            picture: this.imageId,
+            location: this.location,
+            category: this.category,
+            description: this.description,
+          },
+        },
+      });
+    } else {
+      //TODO
     }
 
-    imageChanged(id: string) {
-        this.imageId = id
-    }
-
-    deleteImage() {
-        this.imageId = null
-    }
-
-    pickerUpdated(coordinates: [number, number]) {
-        this.location = coordinates
-    }
-
-    async save(): Promise<void> {
-        if (this.offeringId == null) {
-            await this.$apollo.mutate({
-                mutation: gql`mutation ($input: FoodOfferingInput!) {
-                    createFoodOffering(input: $input) {
-                        id
-                    }
-                }
-                `,
-                variables: {
-                    input: {
-                        title: this.title,
-                        picture: this.imageId,
-                        location: this.location,
-                        category: this.category,
-                        description: this.description
-                    }
-                }
-            })
-        } else {
-            //TODO
-        }
-
-        this.$router.back()
-    }
-
+    this.$router.back();
+  }
 }
 </script>
 <style scoped>
 .save-button {
-    float: right;
+  float: right;
 }
 
 .location-picker {
-    width: 100%;
-    aspect-ratio: 1.4;
+  width: 100%;
+  aspect-ratio: 1.4;
 }
 </style>
